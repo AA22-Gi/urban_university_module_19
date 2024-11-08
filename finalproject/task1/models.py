97aa0827450807978d259a859ceac9ef3b48333a
AA22_Gi <gibert22@mail.ru>
2024-11-08 10:41:05 +0700

    comments

--- finalproject/task1/models.py
+++ finalproject/task1/models.py
@@ -4,11 +4,16 @@ from django.db import models
 # Create your models here.
 class Buyer(models.Model):
     name = models.CharField(max_length=100)  # Имя покупателя
-    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс max_digits
     age = models.PositiveIntegerField()  # Возраст
 
     def __str__(self):
         return self.name
+    #  max_digits=10 общее количество цифр, которые могут быть сохранены в поле,
+    #  включая как целую часть, так и дробную.
+    #  decimal_places=2 - количество цифр, которое может быть сохранено после десятичной точки
+    #  models.PositiveIntegerField() — это тип поля, который используется для хранения целых чисел,
+    #  которые должны быть положительными.
 
 
 class Game(models.Model):
@@ -18,6 +23,10 @@ class Game(models.Model):
     description = models.TextField()  # Описание
     age_limited = models.BooleanField(default=False)  # Ограничение возраста
     buyers = models.ManyToManyField(Buyer, related_name='games')  # Покупатели
-
+    #  models.ManyToManyField(Buyer, related_name='games') — это поле,
+    #  которое устанавливает связь "многие ко многим" между моделями.
     def __str__(self):
         return self.title
+
+#  python manage.py makemigrations task1 - создаем миграцию
+#  python manage.py migrate -  применяем миграцию
