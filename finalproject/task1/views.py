b3a9002cc17a6f3336997aef640d499de3b7a616
AA22_Gi <gibert22@mail.ru>
2024-11-10 09:24:22 +0700

    import UserRegister init: def platform def shop def cart def sign_up_by_django users

--- finalproject/task1/views.py
+++ finalproject/task1/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from .forms import UserRegister
+
 
 # Create your views here.
+def platform(request):
+    return render(request, 'fourth_task/platform.html')
+
+
+def shop(request):
+    games = ['Atomic Heart', 'Cyberpunk 2077', 'PayDay 2']
+
+    context = {
+        'games': games
+    }
+    return render(request, 'fourth_task/games.html', context)
+
+
+def cart(request):
+    return render(request, 'fourth_task/cart.html')
+
+
+users = ['user1', 'user2', 'user3']
+
+
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if username in users:
+                info['error'] = 'Пользователь уже существует'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            else:
+                users.append(username)
+                info['message'] = f'Приветствуем, {username}!'
+        else:
+            form = UserRegister()
+            info['form'] = form
+    else:
+        form = UserRegister()
+        info['form'] = form
+    return render(request, 'fifth_task/registration_page.html', info)
