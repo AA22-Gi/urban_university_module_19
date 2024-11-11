2ee59eef86a0ccef4f2f234245de091d6b5416cb
AA22_Gi <gibert22@mail.ru>
2024-11-11 14:43:15 +0700

    Импортирована модель Post Импортирован Paginator Функция post_list

--- finalproject/pangination/views.py
+++ finalproject/pangination/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.core.paginator import Paginator
 # Create your views here.
+
+
+def post_list(request):
+    post_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(post_list, 5)  # 5 постов на странице
+
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    return render(request, 'pangination/post_list.html', {'posts': posts, 'paginator': paginator})
