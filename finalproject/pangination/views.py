e07a2da1e5aae50043195312effbb099b90495ee
AA22_Gi <gibert22@mail.ru>
2024-11-11 16:08:14 +0700

    Функция post_list_2

--- finalproject/pangination/views.py
+++ finalproject/pangination/views.py
@@ -12,3 +12,19 @@ def post_list(request):
     posts = paginator.get_page(page_number)
 
     return render(request, 'pangination/post_list.html', {'posts': posts, 'paginator': paginator})
+
+
+def post_list_2(request):
+    post_list = Post.objects.all().order_by('created_at')
+
+    items_per_page = request.GET.get('items_per_page', 3)  # По умолчанию 5 постов на странице
+    paginator = Paginator(post_list, items_per_page)  # Используем выбранное количество постов
+
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    return render(request, 'pangination/post_list_2.html', {
+        'posts': posts,
+        'paginator': paginator,
+        'items_per_page': items_per_page
+    })
