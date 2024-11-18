0b9d14f7624cbeda7fd17d1e25d5c92d8a15cb72
AA22_Gi <gibert22@mail.ru>
2024-11-18 14:04:22 +0700

    расширил настройки Buyer и Game в админ панели при помощи класса Meta

--- finalproject/task1/admin.py
+++ finalproject/task1/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Buyer, Game
-# Register your models here.
 
-admin.site.register([Buyer, Game])
+
+# Регистрация модели Buyer
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')  # Отображение полей в списке
+    search_fields = ('name',)  # Поля для поиска
+    list_filter = ('age',)  # Фильтрация по возрасту
+
+# Регистрация модели Game
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited')  # Отображение полей в списке
+    search_fields = ('title',)  # Поля для поиска
+    list_filter = ('age_limited',)  # Фильтрация по ограничению возраста
